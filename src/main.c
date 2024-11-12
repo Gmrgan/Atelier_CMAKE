a3504968b2569c3a375fa028b59827941696ca81
Gmrgan <115547191+Gmrgan@users.noreply.github.com>
2024-11-12 15:28:48 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,30 +4,34 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4 || (argc == 3 && strcmp(argv[1], "sq") == 0))
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sq") == 0) {
-		r = _sq(atof(a));
-		printf("%lf", r);
+	if (argc == 4 || (argc == 3 && strcmp(argv[1], "sq") == 0)) {
+        char* op = argv[1];
+        char* a = argv[2];
+        double r = 0;
+
+        if (strcmp(op, "add") == 0 && argc == 4) {
+            char* b = argv[3]; // Déclaration de 'b' ici
+            r = _add(atof(a), atof(b));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "sub") == 0 && argc == 4) {
+            char* b = argv[3]; // Déclaration de 'b' ici
+            r = _sub(atof(a), atof(b));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "mul") == 0 && argc == 4) {
+            char* b = argv[3]; // Déclaration de 'b' ici
+            r = _mul(atof(a), atof(b));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "div") == 0 && argc == 4) {
+            char* b = argv[3]; // Déclaration de 'b' ici
+            r = _div(atof(a), atof(b));
+            printf("%lf\n", r);
+        }
+        else if (strcmp(op, "sq") == 0 && argc == 3) {
+            r = _sq(atof(a));
+            printf("%lf\n", r);
 	}
 	else {printf("Erreur de parametres");}
 	}
