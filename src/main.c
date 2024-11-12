9671693b9ea8c6b1f1f015250c8ee064d9002e31
Gmrgan <115547191+Gmrgan@users.noreply.github.com>
2024-11-12 15:24:16 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,11 +4,10 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
+	if (argc == 4 || (argc == 3 && strcmp(argv[1], "sq") == 0))
 	{
 	char* op = argv[1];
 	char* a = argv[2];
-	char* b = argv[3];
 	double r = 0;
 	if (strcmp(op, "add") == 0) {
 		r = _add(atof(a), atof(b));
